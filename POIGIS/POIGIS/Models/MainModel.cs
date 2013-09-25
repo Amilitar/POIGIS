67f02a27a14459cd5d65c6b0c4379077a9aafb79
Àáäðàøèòîâ Íèêèòà <nikpodrivnik@mail.ru>
2013-09-25 23:05:05 +0700

    Просто так

---
+++ POIGIS/POIGIS/Models/MainModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POIGIS.Models
+{
+    public class MainModel
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+}
